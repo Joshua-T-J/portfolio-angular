33ec36290205111fb413b1914f5884112449bb9f
Joshua T J <joshuajose312@gmail.com>
2024-03-02 13:01:25 +0530

    logo resized

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -24,17 +24,12 @@ const fadeAnimation = [
   ]),
 ];
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css'],
-    animations: [trigger('fadeInOut', fadeAnimation)],
-    standalone: true,
-    imports: [
-        NgIf,
-        NgFor,
-        MatTooltipModule,
-        SlicePipe,
-    ],
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  animations: [trigger('fadeInOut', fadeAnimation)],
+  standalone: true,
+  imports: [NgIf, NgFor, MatTooltipModule, SlicePipe],
 })
 export class HomeComponent implements OnInit {
   toRotate: string[] = [
